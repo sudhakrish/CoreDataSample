145fae6981049f5cf4ddcd881cb34bd7dfaf9483
Sudhakar Krishnan <iamsudhakrish@gmail.com>
2020-08-20 15:55:16 +0800

    initial project added with core data

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by sudhakar krishnan on 20/8/20.
+//  Copyright © 2020 sudhakar krishnan. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
